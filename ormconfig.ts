22de174927223f1eb3aed552c9860907014273b8
Alexandr Kasyan <gumoni.33@mail.ru>
2025-04-26 20:40:33 +0300

    migration try1

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,11 @@
     // ormconfig.js
     module.exports = {
         type: 'postgres',
-        host: 'localhost',  // Или IP адрес твоего сервера PostgreSQL
-        port: 5050,
-        username: 'postgres', // Или postgres, если не создавал отдельного пользователя
-        password: 'admin', // Твой пароль
-        database: 'wedding_invite',
+        host: 'dpg-d06co9s9c44c73febfog-a',  // Или IP адрес твоего сервера PostgreSQL
+        port: 5432,
+        username: 'postgresql', // Или postgres, если не создавал отдельного пользователя
+        password: 'NjFOSdP08dYmeax87rRfhonaa2tdQrCQ', // Твой пароль
+        database: 'wedding_test_cgzw',
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: false, // Важно: В production должно быть false! Используй migrations.
         migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
