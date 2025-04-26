df9b09db9948bd24f6bf4acc9f5fc0b66dcc5be6
Alexandr Kasyan <gumoni.33@mail.ru>
2025-04-26 16:41:53 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      process.env.FRONT_URL,  // URL фронтенда
+      'http://localhost:3000',               // Для локальной разработки
+    ],
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
