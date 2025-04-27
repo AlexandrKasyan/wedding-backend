8847448d7bc3e7e7eddaef3c93da27eb222a36ef
Alexandr Kasyan <gumoni.33@mail.ru>
2025-04-27 20:39:46 +0300

    try14

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,14 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
+import { Guest } from './guest/guest.entity';
 
 config();
 
 export default new DataSource({
     type: 'postgres',
     url: 'postgresql://postgresql:NjFOSdP08dYmeax87rRfhonaa2tdQrCQ@dpg-d06co9s9c44c73febfog-a.oregon-postgres.render.com/wedding_test_cgzw?sslmode=require',
-    migrations: ['dist/migrations/*.js'],
-    entities: ['dist/src/**/*.entity.js'],
-    ssl: {
-        rejectUnauthorized: false
-    },
-    extra: {
-        ssl: {
-            require: true
-        }
-    }
+    entities: [Guest],
+    migrations: [__dirname + '/migrations/*{.js,.ts}'],
+    ssl: { rejectUnauthorized: false },
+    synchronize: false // Важно для production!
 });
