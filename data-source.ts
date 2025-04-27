9946ad94556c1a6a00b9db0bab2b117bc92798bc
Alexandr Kasyan <gumoni.33@mail.ru>
2025-04-27 13:34:01 +0300

    try11

--- data-source.ts
+++ data-source.ts
@@ -7,7 +7,7 @@ export default new DataSource({
   type: 'postgres',
   url: 'postgresql://postgresql:NjFOSdP08dYmeax87rRfhonaa2tdQrCQ@dpg-d06co9s9c44c73febfog-a.oregon-postgres.render.com/wedding_test_cgzw?sslmode=require',
   entities: ['dist/src/**/*.entity.js'],
-  migrations: ['dist/migrations/*.js'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'], 
   ssl: {
     rejectUnauthorized: false
   }
