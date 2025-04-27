bb5ee31138e383de7cff5225a144c82b6a631bfa
Alexandr Kasyan <gumoni.33@mail.ru>
2025-04-27 13:25:28 +0300

    екн10

--- data-source.ts
+++ data-source.ts
@@ -6,7 +6,7 @@ config();
 export default new DataSource({
   type: 'postgres',
   url: 'postgresql://postgresql:NjFOSdP08dYmeax87rRfhonaa2tdQrCQ@dpg-d06co9s9c44c73febfog-a.oregon-postgres.render.com/wedding_test_cgzw?sslmode=require',
-  entities: ['dist/**/*.entity.js'],
+  entities: ['dist/src/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   ssl: {
     rejectUnauthorized: false
