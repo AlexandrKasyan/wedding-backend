502ff379c0dc419741ec048523c4cbfc0f612817
Alexandr Kasyan <gumoni.33@mail.ru>
2025-04-27 20:20:44 +0300

    try12

--- data-source.ts
+++ data-source.ts
@@ -4,11 +4,16 @@ import { config } from 'dotenv';
 config();
 
 export default new DataSource({
-  type: 'postgres',
-  url: 'postgresql://postgresql:NjFOSdP08dYmeax87rRfhonaa2tdQrCQ@dpg-d06co9s9c44c73febfog-a.oregon-postgres.render.com/wedding_test_cgzw?sslmode=require',
-  entities: ['dist/src/**/*.entity.js'],
-  migrations: ["dist/migrations/*.js"], 
-  ssl: {
-    rejectUnauthorized: false
-  }
+    type: 'postgres',
+    url: 'postgresql://postgresql:NjFOSdP08dYmeax87rRfhonaa2tdQrCQ@dpg-d06co9s9c44c73febfog-a.oregon-postgres.render.com/wedding_test_cgzw?sslmode=require',
+    migrations: ['dist/migrations/*.js'],
+    entities: ['dist/src/**/*.entity.js'],
+    ssl: {
+        rejectUnauthorized: false
+    },
+    extra: {
+        ssl: {
+            require: true
+        }
+    }
 });
